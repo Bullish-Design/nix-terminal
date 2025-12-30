383ccb3aa4cf48defc07a1c9bc54b7a6cd5a870b
Bullish-Design <97364799+Bullish-Design@users.noreply.github.com>
2025-12-29 19:33:34 -0500

    Update terminal.nix to include tree and jq

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,8 +1,16 @@
+{ nvim-config }:
 { pkgs, ... }:
 {
+  imports = [
+    nvim-config.homeManagerModules.default
+  ];
+
+  # Minimal terminal “profile” extras
   programs.git.enable = true;
-  home.packages = with pkgs; [ 
-    ripgrep 
-    fd 
+
+  home.packages = with pkgs; [
+    tree
+    jq
   ];
 }
+
